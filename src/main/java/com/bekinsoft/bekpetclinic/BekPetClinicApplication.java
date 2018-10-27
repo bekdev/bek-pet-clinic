1a17282d605ce183c168b3539c1531f4fc1acbfe
bekdev <bekintest@gmail.com>
2018-10-27 18:44:32 +0100

    Initial Project Add

---
+++ src/main/java/com/bekinsoft/bekpetclinic/BekPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.bekinsoft.bekpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BekPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BekPetClinicApplication.class, args);
+	}
+}
