40a8c6dd5a95f95bde65d63dab27ce6ad53d9f8c
bekdev <bekintest@gmail.com>
2018-11-03 15:12:59 +0000

    Implement Interfaces for services. Closes #5

---
+++ bek-pet-clinic-data/src/main/java/com/bekinsoft/bekpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.bekinsoft.bekpetclinic.services;
+
+import com.bekinsoft.bekpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
