0a4d4aa270c3502a30c6e661a93e0eba330bb85e
bekdev <bekintest@gmail.com>
2018-11-07 09:37:49 +0000

    refactoring to common crud interface. closes #11

--- bek-pet-clinic-data/src/main/java/com/bekinsoft/bekpetclinic/services/VetService.java
+++ bek-pet-clinic-data/src/main/java/com/bekinsoft/bekpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.bekinsoft.bekpetclinic.services;
 
 import com.bekinsoft.bekpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
