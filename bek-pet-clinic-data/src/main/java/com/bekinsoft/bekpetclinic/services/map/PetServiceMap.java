00c58fd00e495760d8545d81e746c4807be465bf
bekdev <bekintest@gmail.com>
2018-11-07 10:18:31 +0000

    implement map based service. closes #6

---
+++ bek-pet-clinic-data/src/main/java/com/bekinsoft/bekpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.bekinsoft.bekpetclinic.services.map;
+
+import com.bekinsoft.bekpetclinic.model.Pet;
+import com.bekinsoft.bekpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
